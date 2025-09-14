43748fd06f1cb62c8c8dc6ed68020c7e62a89178
shadowseek23 <shadowsyd_services@protonmail.com>
2025-09-14 21:19:03 +0200

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,36 @@ const config = {
         sans: ["var(--font-sans)"],
         mono: ["var(--font-mono)"],
       },
+      colors: {
+        stroke: "#616161",
+      },
+      letterSpacing: {
+        'kapitalek': '0.13em',
+      },
     },
   },
   darkMode: "class",
-  plugins: [heroui()],
-}
+  plugins: [heroui({
+    "themes": {
+      "light": {
+      },
+      "dark": {
+      }
+    },
+    "layout": {
+      "disabledOpacity": "0.2",
+      "radius": {
+        "small": "0px",
+        "medium": "0px",
+        "large": "0px",
+      },
+      "borderWidth": {
+        "small": "1px",
+        "medium": "1px",
+        "large": "1px",
+      },
+    }
+  })],
+};
 
 module.exports = config;
