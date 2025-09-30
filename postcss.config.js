5387347727a15275e13872091ad628eca598cdeb
shadowseek23 <shadowsyd_services@protonmail.com>
2025-09-30 18:19:50 +0200

    post css plugins + npm react icons

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   plugins: {
+    "postcss-import": {},
+    "postcss-mixins": {},
+    "postcss-simple-vars": {},
+    "postcss-nested": {},
+    "postcss-utilities": {},
     "@tailwindcss/postcss": {},
   },
 }
