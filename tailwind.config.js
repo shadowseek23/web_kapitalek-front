f747b810c868cc992f85b20163e3806b09ef7e11
shadowseek23 <shadowsyd_services@protonmail.com>
2025-10-09 22:56:03 +0200

    config common js - je to spravne???

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,18 @@
-import {heroui} from "@heroui/theme"
-
 /** @type {import('tailwindcss').Config} */
-const config = {
+module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    // './components/**/*.{js,ts,jsx,tsx,mdx}',
+    // './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    // './styles/**/*.css'
   ],
+
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
-      },
-      colors: {
-        stroke: "#616161",
-      },
-      letterSpacing: {
-        'kapitalek': '0.13em',
-      },
+    container: {
+      center: true,
     },
+    extend: {},
   },
-  darkMode: "class",
-  plugins: [heroui({
-    "themes": {
-      "light": {
-      },
-      "dark": {
-      }
-    },
-    "layout": {
-      "disabledOpacity": "0.2",
-      "radius": {
-        "small": "0px",
-        "medium": "0px",
-        "large": "0px",
-      },
-      "borderWidth": {
-        "small": "1px",
-        "medium": "1px",
-        "large": "1px",
-      },
-    }
-  })],
+  plugins: [],
 };
 
-module.exports = config;
