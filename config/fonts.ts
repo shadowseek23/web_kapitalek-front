0edeb9e9356678a99a5fe120e2f670741393d031
shadowseek23 <shadowsyd_services@protonmail.com>
2025-10-09 22:57:03 +0200

    js breakpoints and fix fonts swap

--- config/fonts.ts
+++ config/fonts.ts
@@ -3,9 +3,13 @@ import { Fira_Code as FontMono, Geologica as FontSans } from "next/font/google";
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
+  display: "swap",
+  preload: true,
 });
 
 export const fontMono = FontMono({
   subsets: ["latin"],
   variable: "--font-mono",
+  display: "swap",
+  preload: true,
 });
